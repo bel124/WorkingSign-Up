ed3a549c991a9da235f7ca1075f3a35360a86c91
bel124 <mferdinan124@hotmail.com>
2020-10-01 19:23:21 -0400

    renamed my html file and updated the js one

--- SignUp.js
+++ SignUp.js
@@ -1,6 +1,18 @@
 function validfname() {
     var fn = document.getElementById("fname").value;
+    var ln = documnet.getElementById("lname").value;
+    var ht = document.getElementById("town").value;
+    var pro = document.getElementById("Profession").value;
 
-    if(fn !== String)
+    if (fn && ln && ht && pro !== String)
         document.getElementById("output").textContent = "Input is not valid. Only text is allowed. Any other characters are invalid, such as numbers and '+*/!@#$%^&*(), etc."
+}
+
+function ValidateEmail(mail) {
+        if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(SignUp1.email.value))
+        {
+            return (true)
+        }
+        alert("You have entered an invalid email address!")
+        return (false)
 }
