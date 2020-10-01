8a8c70f4f2e3bb29dbe2d07ba0d8f18caf731fb6
bel124 <mferdinan124@hotmail.com>
2020-10-01 17:46:10 -0400

    fixed the variable for fname in if statement

--- SignUp.js
+++ SignUp.js
@@ -1,6 +1,6 @@
 function validfname() {
     var fn = document.getElementById("fname").value;
 
-    if(val !== String)
+    if(fn !== String)
         document.getElementById("output").textContent = "Input is not valid. Only text is allowed. Any other characters are invalid, such as numbers and '+*/!@#$%^&*(), etc."
 }
