6a865c395f397f46ef6062a4df63aae0318260d3
bel124 <mferdinan124@hotmail.com>
2020-10-01 23:46:17 -0400

    fixed if statement

--- SignUp.js
+++ SignUp.js
@@ -6,10 +6,9 @@ function validfname() {
 
 
 
-    if (fn !== String)
+    if (fn !== String) {
         document.getElementById("output").textContent = "Input is not valid. Only text is allowed. Any other characters are invalid, such as numbers and '+*/!@#$%^&*(), etc.";
-
-
+    }
 }
 
 function validation(){
