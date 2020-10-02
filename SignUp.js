1f00557351be45e21a30527d3990c3d2cc944f2c
bel124 <mferdinan124@hotmail.com>
2020-10-01 23:35:07 -0400

    testing email...

--- SignUp.js
+++ SignUp.js
@@ -3,12 +3,27 @@ function validfname() {
     var ln = document.getElementById("lname").value;
     var ht = document.getElementById("town").value;
     var pro = document.getElementById("Profession").value;
-    var email = document.getElementById("email").value;
+
+
 
     if (fn && ln && ht && pro !== String)
         document.getElementById("output").textContent = "Input is not valid. Only text is allowed. Any other characters are invalid, such as numbers and '+*/!@#$%^&*(), etc.";
+
+
 }
 
-function emailIsValid (email) {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+function validation(){
+    var form = document.getElementById("form");
+    var email = document.getElementById("email").value;
+    var text = document.getElementById("text");
+    var pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(email.match(pattern)) {
+        form.classList.add("valid");
+        form.classList.remove("invalid");
+    }
+    else {
+        form.classList.remove("valid");
+        form.classList.add("invalid");
+    }
 }
