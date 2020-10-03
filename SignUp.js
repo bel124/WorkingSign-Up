d1f78d7454a44baed2a05e58b9349328ec459a7d
bel124 <mferdinan124@hotmail.com>
2020-10-02 23:45:20 -0400

    fixed email function

--- SignUp.js
+++ SignUp.js
@@ -4,33 +4,27 @@ function validfname() {
     var ht = document.getElementById("town").value;
     var pro = document.getElementById("Profession").value;
 
-for (var i = 0; i <64; i++){
-    if (isNaN(validfname())) {
-        document.getElementById("output").textContent = "Input is valid";
-    } else {
-        document.getElementById("output").textContent = "Input is not valid. Only text is allowed. Any other characters are invalid, such as numbers and '+*/!@#$%^&*(), etc.";
-    }
-}
-
-function validEmail(inputText){
-    var em = document.getElementById("email").value;
-    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        if(inputText.value.match(mailformat))
-        {
-            alert("Valid email address!");
-            document.form.email.focus();
-            return true;
+    for (var i = 0; i < 64; i++) {
+        if (isNaN(validfname())) {
+            document.getElementById("output").textContent = "Input is valid";
+        } else {
+            document.getElementById("output").textContent = "Input is not valid. Only text is allowed. Any other characters are invalid, such as numbers and '+*/!@#$%^&*(), etc.";
         }
-        else
-        {
-            alert("You have entered an invalid email address!");
-            document.form.email.focus();
-            return false;
     }
 }
- //do not know why function validEmail is being 'unused'
 
+function validEmail(email){
+
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if(mailformat.test(email.value) ==false){
+            alert('Invalid email address. Please try again!');
+            return(false);
+        }
+        return true;
 }
+       //do not know why function validEmail is being 'unused'
+
+
 
     function cancel(){
         document.getElementById("form").reset();
